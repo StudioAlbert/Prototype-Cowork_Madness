ee6b47f32eb7169b74fb01f1069da562675ab671
StudioAlbert <account@studio-albert.com>
2024-12-20 23:50:31 +0100

    Render Character controller

--- CoworkMadness-UnityProject/Assets/05 - Scripts/Characters/PlayerMovement.cs
+++ CoworkMadness-UnityProject/Assets/05 - Scripts/Characters/PlayerMovement.cs
@@ -5,12 +5,14 @@ public class PlayerMovement : MonoBehaviour
     
     [SerializeField] private float _fwdSpeed = 1.0f;
     [SerializeField] private float _rotationSpeed = 25.0f;
+    [SerializeField] private Transform _moveRoot;
+    [SerializeField] private Animator _animator;
     
     // Components
     private PlayerInputController _inputController;
     private CharacterController _characterController;
     private GesturesManager _gesturesManager;
-    [SerializeField] private Animator _animator;
+    
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -26,8 +28,8 @@ public class PlayerMovement : MonoBehaviour
         if(_inputController.Movement >= 0)
         {
             _gesturesManager?.StopPlayGesture();
-            _characterController.SimpleMove(transform.forward * (_inputController.Movement * _fwdSpeed * Time.deltaTime));
-            _characterController.transform.Rotate(Vector3.up, _inputController.Rotation * _rotationSpeed * Time.deltaTime);
+            //_characterController.SimpleMove(transform.forward * (_inputController.Movement * _fwdSpeed * Time.deltaTime));
+            _moveRoot.Rotate(Vector3.up, _inputController.Rotation * _rotationSpeed * Time.deltaTime);
         }
         else
         {
