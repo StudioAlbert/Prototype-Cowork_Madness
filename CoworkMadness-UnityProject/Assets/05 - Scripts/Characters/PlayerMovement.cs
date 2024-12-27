5d51a79a090f94cf618605924711378adcce4124
StudioAlbert <account@studio-albert.com>
2024-12-28 00:50:08 +0100

    Player movement

--- CoworkMadness-UnityProject/Assets/05 - Scripts/Characters/PlayerMovement.cs
+++ CoworkMadness-UnityProject/Assets/05 - Scripts/Characters/PlayerMovement.cs
@@ -9,21 +9,22 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float _turnBackTime = 0.3f;
     [SerializeField] private Transform _moveRoot;
     [SerializeField] private Animator _animator;
-
+    [SerializeField] private CharacterController _characterController;
+    
     // Components
     private PlayerInputController _inputController;
-    private CharacterController _characterController;
+    
     private GesturesManager _gesturesManager;
     private Camera _mainCamera;
     private float _targetRotation;
-   
+    float _rotationVelocity = 0f;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         _inputController = GetComponent<PlayerInputController>();
-        _characterController = GetComponent<CharacterController>();
+        //_characterController = GetComponent<CharacterController>();
         _gesturesManager = GetComponentInChildren<GesturesManager>();
         _mainCamera = Camera.main;
     }
@@ -38,24 +39,27 @@ public class PlayerMovement : MonoBehaviour
         
         // normalise input direction
         Vector3 inputDirection = new Vector3(_inputController.Movement.x, 0.0f, _inputController.Movement.y).normalized;
-
-        if (_inputController.Movement != Vector2.zero)
+        float inputMagnitude = inputDirection.magnitude;
+        
+        if (inputMagnitude > 0.01)
         {
             _targetRotation = Mathf.Atan2(inputDirection.x, inputDirection.z) * Mathf.Rad2Deg +
                               _mainCamera.transform.eulerAngles.y;
-            
-            float _rotationVelocity = 0f;
-            float rotation = Mathf.SmoothDampAngle(transform.eulerAngles.y, _targetRotation, ref _rotationVelocity,
-                RotationSmoothTime);
-
-            // rotate to face input direction relative to camera position
-            _moveRoot.rotation = Quaternion.Euler(0.0f, rotation, 0.0f);
         }
+        float rotation = Mathf.SmoothDampAngle(_moveRoot.eulerAngles.y,
+            _targetRotation,
+            ref _rotationVelocity,
+            RotationSmoothTime);
 
-        //_moveRoot.LookAt(_moveRoot.position + new Vector3(_inputController.Movement.x, 0, _inputController.Movement.y));
-        
-        
-        _animator.SetFloat(AnimatorHandles.WalkSpeed, _inputController.Movement.magnitude);
+        // rotate to face input direction relative to camera position
+        _moveRoot.rotation = Quaternion.Euler(0.0f, rotation, 0.0f);
 
+        if (inputMagnitude > 0.01)
+        {
+            // move the player
+            Vector3 targetDirection = Quaternion.Euler(0.0f, _targetRotation, 0.0f) * Vector3.forward * (_fwdSpeed * inputMagnitude);
+            _characterController.SimpleMove(targetDirection);
+        }
+        _animator.SetFloat(AnimatorHandles.WalkSpeed, inputMagnitude);
     }
 }
