b4bebf3cc1550826ceeb454994a90f6284573c2f
StudioAlbert <account@studio-albert.com>
2024-12-28 00:17:05 +0100

    add cinemachine + TPS controller

--- CoworkMadness-UnityProject/Assets/05 - Scripts/Characters/PlayerMovement.cs
+++ CoworkMadness-UnityProject/Assets/05 - Scripts/Characters/PlayerMovement.cs
@@ -1,11 +1,11 @@
 using UnityEngine;
-using Quaternion = System.Numerics.Quaternion;
 
 public class PlayerMovement : MonoBehaviour
 {
 
     [SerializeField] private float _fwdSpeed = 1.0f;
     [SerializeField] private float _rotationSpeed = 25.0f;
+    [SerializeField] private float RotationSmoothTime;
     [SerializeField] private float _turnBackTime = 0.3f;
     [SerializeField] private Transform _moveRoot;
     [SerializeField] private Animator _animator;
@@ -14,6 +14,9 @@ public class PlayerMovement : MonoBehaviour
     private PlayerInputController _inputController;
     private CharacterController _characterController;
     private GesturesManager _gesturesManager;
+    private Camera _mainCamera;
+    private float _targetRotation;
+   
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -22,18 +25,34 @@ public class PlayerMovement : MonoBehaviour
         _inputController = GetComponent<PlayerInputController>();
         _characterController = GetComponent<CharacterController>();
         _gesturesManager = GetComponentInChildren<GesturesManager>();
+        _mainCamera = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
         if (_inputController.Movement.magnitude > 0)
-
             _gesturesManager?.StopPlayGesture();
         else
             _gesturesManager?.StartPlayGesture();
+        
+        // normalise input direction
+        Vector3 inputDirection = new Vector3(_inputController.Movement.x, 0.0f, _inputController.Movement.y).normalized;
 
-        _moveRoot.LookAt(_moveRoot.position + new Vector3(_inputController.Movement.x, 0, _inputController.Movement.y));
+        if (_inputController.Movement != Vector2.zero)
+        {
+            _targetRotation = Mathf.Atan2(inputDirection.x, inputDirection.z) * Mathf.Rad2Deg +
+                              _mainCamera.transform.eulerAngles.y;
+            
+            float _rotationVelocity = 0f;
+            float rotation = Mathf.SmoothDampAngle(transform.eulerAngles.y, _targetRotation, ref _rotationVelocity,
+                RotationSmoothTime);
+
+            // rotate to face input direction relative to camera position
+            _moveRoot.rotation = Quaternion.Euler(0.0f, rotation, 0.0f);
+        }
+
+        //_moveRoot.LookAt(_moveRoot.position + new Vector3(_inputController.Movement.x, 0, _inputController.Movement.y));
         
         
         _animator.SetFloat(AnimatorHandles.WalkSpeed, _inputController.Movement.magnitude);
