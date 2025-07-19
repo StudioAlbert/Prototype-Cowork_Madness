a4bc49867bd65f885b86fce18493f1586f5b8d3d
StudioAlbert <account@studio-albert.com>
2025-07-19 23:08:47 +0200

    Modern Behavior Tree from unity Package

---
+++ CoworkMadness-UnityProject/Assets/05 - Scripts/Characters/AI/Goal.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+[Serializable]
+class Mood
+{
+    public GoalType Type { get; set; }
+    public float Priority { get; set; }
+    public Mood(GoalType type, float priority)
+    {
+        Type = type;
+        Priority = priority;
+    }
+}
